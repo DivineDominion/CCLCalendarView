1ea5b9a9e9564dc039fd7a43cb8358e53cbbd5f4
Christian Tietze <christian.tietze@gmail.com>
2015-10-24 11:02:56 +0200

    import Cocoa where appropriate

--- CalendarView/CCLCalendarViewDelegate.h
+++ CalendarView/CCLCalendarViewDelegate.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Christian Tietze. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 @protocol CCLCalendarViewDelegate <NSObject>
 - (void)tableView:(NSTableView *)tableView didSelectCellViewAtRow:(NSInteger)row column:(NSInteger)column;
--- CalendarView/CCLProvidesDetailView.h
+++ CalendarView/CCLProvidesDetailView.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Christian Tietze. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 @protocol CCLProvidesDetailView <NSObject>
 @required
